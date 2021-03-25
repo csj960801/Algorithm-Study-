8aae77b3a285b8d26201c0e5b0fdfa8c00746b9d
CSJ <CSJ@Worth-Maker-Gensis>
2021-03-25 14:26:15 +0900

    파이썬 코드100제 3/25

---
+++ Algorithm공부/src/코드100제/21-3-25/10진 정수 입력받아 16진수로 출력하기1.py
@@ -0,0 +1,4 @@
+a = input()
+h = int(a)
+#h에 저장되어있는 값을 16진수(hexadecimal) 소문자 형태 문자열로 출력
+print('%x'%h)
---
+++ Algorithm공부/src/코드100제/21-3-25/정수 2개 입력받아 나눈 나머지 계산하기.py
@@ -0,0 +1,5 @@
+#python언어에서는 나눈 몫을 계산하는 연산자(//, floor division)를 제공한다.
+#a//b 와 같이 작성하면, a를 b로 나눈 몫(quotient)을 계산해준다.
+a,b = input().split()
+c = int(a) % int(b)
+print(c)
